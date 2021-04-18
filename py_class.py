d958ef39c6806b4bddaae7a1e3ac488b750e471f
malai12 <thirumohan2001@gmail.com>
2021-04-18 12:42:17 +0530

    OOPs

---
+++ py_class.py
@@ -0,0 +1,46 @@
+class supermarket:
+    '''this is document string''' # documentation string_
+    # __init__ is called automatically when the object is created
+    # it is a constructor.it is a special function.it is used to initialize object specific information
+
+    owner = "human"   #class specific variable
+    worker  = "Human" #class specific variable
+    def __init__(self,brand,price,discount): # formal parameter
+
+        self.brand = brand
+        self.price = price
+        self.dis = discount
+
+# print(love.__doc__) --> it prints the document of a class
+
+# help(love) --> it gives information about class
+
+# ball = supermarket() # --> object creation
+# ball.type = "stumper"
+# ball.price = 40
+
+# biscuit = supermarket()
+# biscuit.brand = "milk"
+# biscuit.price = 10
+
+# print(ball.price) #--> prints price of ball
+# print(ball.__dict__) # --> gives all the specification of ball in dictionary
+
+bread = supermarket("xyz",25,25) # actual parameter
+print(bread.dis)
+biscuit = supermarket("milk",10,3)
+print(biscuit.__dict__)
+soap = supermarket("wash",10,1)
+print(soap.__dict__)
+
+# object specific variable self -->multiple copies will be maintained like brand of biscuit,soap,etc
+# class specific variable --> only one copy will be maintained
+
+print(supermarket.owner) #to access class variable
+print(supermarket.worker)
+
+# I want to change the owner of soap so from human to man so the code below
+
+soap.owner = "man"
+print(soap.owner)
+print(biscuit.owner)
