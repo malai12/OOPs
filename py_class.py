131da3023d9dd89ab0e0ffc05662476197faf7e4
malai12 <thirumohan2001@gmail.com>
2021-04-24 11:29:22 +0530

    updated

--- py_class.py
+++ py_class.py
@@ -36,13 +36,67 @@ print(soap.__dict__)
 # object specific variable self -->multiple copies will be maintained like brand of biscuit,soap,etc
 # class specific variable --> only one copy will be maintained
 
-print(supermarket.owner) #to access class variable
-print(supermarket.worker)
+# print(supermarket.owner) #to access class variable
+# print(supermarket.worker)
 
 # I want to change the owner of soap so from human to man so the code below
 
 soap.owner = "man"
-print(soap.owner)
-print(biscuit.owner)
+# print(soap.owner)
+# print(biscuit.owner)
 
-#for git
+#for gitgit
+
+class bank:
+
+    a = 100
+    def deposit(self,amout): # amout is local varibale. it is used in this method only
+        print("Amount to be deposited: ",amout)
+
+thiru = bank()
+thiru.deposit(500)
+thiru.a = 200
+print(thiru.a)
+
+'''
+Functions - Methods - 3types - 
+1 instance method - sample - memory reference - object
+   instance variable - object specific variable  - self(keyword)
+2 class method - cls is keyword
+    - inside the function definition class specific variable only used
+    - @classmethod decorator is used
+    - cls varible is used
+    - can call class name by using class name or object reference
+3 static method - utility method 
+    - @staticmethod decorator is used
+    - it is not object specific and class specific method 
+    - local variable are
+    
+   '''
+
+class student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.mark = marks
+    def show(self):
+        print(self.name,self.mark)  #instance method
+a = student('Thirumalai',100)
+a.show()
+
+class love:
+    a = 10
+    @classmethod
+    def display(cls):
+        print(cls.a)
+
+s = love()
+s.display()
+
+class power:
+    @staticmethod
+    def powerof(x,y): #static method
+        print(x**y)
+
+power.powerof(2,3)
+do = power()
+do.powerof(2,3)
